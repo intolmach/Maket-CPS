ca5b440f3b83cede7a8d0efbc34390f67c00801a
Roman Tolmachev <SilverShadow.irk@gmail.com>
2024-06-09 00:02:59 +0800

    link scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import '../scss/style.scss'
-
+import '../scss/fonts.scss'
+import '../scss/main.scss'
+import '../scss/reset.scss'
+import '../scss/header.scss'
+import '../scss/menu-modal.scss'
+import '../scss/mobile-menu.scss'
+import '../scss/swiper-brands.scss'
+import '../scss/swiper-price.scss'
+import '../scss/swiper-technika.scss'
+import '../scss/footer.scss'
